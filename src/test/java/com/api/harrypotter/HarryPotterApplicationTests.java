b68c788e64c68f20cfbe0ecca1ba32f846acc2f5
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-10-05 19:31:34 -0400

    initial commit

---
+++ src/test/java/com/api/harrypotter/HarryPotterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.harrypotter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarryPotterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
